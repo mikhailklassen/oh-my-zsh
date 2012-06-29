2a91d61ebc79d40d00bf50f096fd287ba01f49dd
Mikhail Klassen <mikhail.klassen@gmail.com>
2012-06-29 11:56:45 -0400

    Merged in the changes from https://gist.github.com/2297824 by user ittayd which should help speed up the svn plugin.

--- plugins/svn/svn.plugin.zsh
+++ plugins/svn/svn.plugin.zsh
@@ -1,5 +1,6 @@
 function svn_prompt_info {
-    if [ $(in_svn) ]; then
+	if in_svn; then
+		unset SVN_INFO
         echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_PREFIX\
 $ZSH_THEME_REPO_NAME_COLOR$(svn_get_repo_name)$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_SVN_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR$(svn_dirty)$ZSH_PROMPT_BASE_COLOR"
     fi
@@ -7,30 +8,35 @@ $ZSH_THEME_REPO_NAME_COLOR$(svn_get_repo_name)$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_S
 
 
 function in_svn() {
-    if [[ -d .svn ]]; then
-        echo 1
-    fi
+	test -d .svn
+}
+
+function svn_get_info() {
+	if [ ! $SVN_INFO ]; then
+		SVN_INFO=$(svn info)
+	fi
+	echo "$SVN_INFO"
 }
 
 function svn_get_repo_name {
-    if [ $(in_svn) ]; then
-        svn info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
-    
-        svn info | sed -n "s/URL:\ .*$SVN_ROOT\///p"
+ 	if in_svn; then
+		svn_get_info | sed -n 's/Repository\ Root:\ .*\///p' | read SVN_ROOT
+
+		svn_get_info | sed -n "s/URL:\ .*$SVN_ROOT\///p" | sed "s/\/.*$//"
     fi
 }
 
 function svn_get_rev_nr {
-    if [ $(in_svn) ]; then
-        svn info 2> /dev/null | sed -n s/Revision:\ //p
+	if in_svn; then
+	    svn_get_info 2> /dev/null | sed -n s/Revision:\ //p
     fi
 }
 
 function svn_dirty_choose {
-    if [ $(in_svn) ]; then
-        s=$(svn status|grep -E '^\s*[ACDIM!?L]' 2>/dev/null)
-        if [ $s ]; then 
-            echo $1
+	if in_svn; then
+	    svn status | grep -q -E '^\s*[ACDIM!?L]' 2>/dev/null
+        if [ $? ]; then             
+			echo $1
         else 
             echo $2
         fi
